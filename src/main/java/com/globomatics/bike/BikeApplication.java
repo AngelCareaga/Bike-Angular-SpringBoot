2953a582362b06df8f62a8c2d232e225b718f32b
Angel Careaga <acareagag@gmail.com>
2018-05-08 13:41:18 -0500

    Autoconfiguration
    - Add anotation Spring.

--- src/main/java/com/globomatics/bike/BikeApplication.java
+++ src/main/java/com/globomatics/bike/BikeApplication.java
@@ -1,32 +1,37 @@
 package com.globomatics.bike;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @PropertySource(value = {"file:${appprop}"})
 public class BikeApplication extends SpringBootServletInitializer {
+
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(BikeApplication.class);
     }
 
-	public static void main(String[] args) {
-	    try {
+    public static void main(String[] args) {
+        try {
             System.out.println("========================================");
             System.out.println("ExtE: " + System.getenv("appprop"));
             System.out.println("ExtP: " + System.getProperty("app_prop"));
             System.out.println("========================================");
-        }catch (Exception e){
-           System.out.println(e);
+        } catch (Exception e) {
+            System.out.println(e);
         }
 
-		SpringApplication.run(BikeApplication.class, args);
-	}
+        SpringApplication.run(BikeApplication.class, args);
+    }
 }
